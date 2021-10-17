58d5ea09d315116b157c67816caaff34f11e16f5
StarsX <X-Universe@outlook.com>
2021-10-17 19:50:49 +0800

    XUSGMachineLearning updated with all base ops

--- SuperResolution/SuperResolutionX.h
+++ SuperResolution/SuperResolutionX.h
@@ -45,9 +45,9 @@ private:
 	XUSG::CommandAllocator::uptr	m_commandAllocators[FrameCount];
 	XUSG::CommandQueue::uptr		m_commandQueue;
 
-	XUSG::Device::sptr				m_device;
-	XUSG::RenderTarget::uptr		m_renderTargets[FrameCount];
-	XUSG::CommandList::uptr			m_commandList;
+	XUSG::Device::sptr			m_device;
+	XUSG::RenderTarget::uptr	m_renderTargets[FrameCount];
+	XUSG::CommandList::uptr		m_commandList;
 
 	// DML
 	std::unique_ptr<SuperResolution> m_superResolution;
